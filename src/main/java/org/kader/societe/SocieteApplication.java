78d6f65d3bbdaf3df01ec6775c6f868ae38d3d85
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2019-07-03 19:07:48 +0200

    first commit

---
+++ src/main/java/org/kader/societe/SocieteApplication.java
@@ -0,0 +1,13 @@
+package org.kader.societe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocieteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocieteApplication.class, args);
+    }
+
+}
