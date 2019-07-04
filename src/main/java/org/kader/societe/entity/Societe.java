8bcc2240c319cc50b91ce0a499be63c84b036289
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2019-07-04 15:49:28 +0200

    maven build jar

--- src/main/java/org/kader/societe/entity/Societe.java
+++ src/main/java/org/kader/societe/entity/Societe.java
@@ -16,5 +16,46 @@ public class Societe implements Serializable {
     @Id @GeneratedValue
     private Long id;
     private String nomSociete;
+    
+    
+    
+	public Societe() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public Societe(Long id, String nomSociete) {
+		super();
+		this.id = id;
+		this.nomSociete = nomSociete;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getNomSociete() {
+		return nomSociete;
+	}
+
+
+
+	public void setNomSociete(String nomSociete) {
+		this.nomSociete = nomSociete;
+	}
+    
+    
 
 }
